c83484a1f15bb716037b243e3ca261750ef201c6
RafiTahmid <rafi.tahmid019@gmail.com>
2022-12-30 15:36:59 +0600

    Brand Carousel added

--- src/Pages/Shared/BrandCarousel/BrandCarousel.js
+++ src/Pages/Shared/BrandCarousel/BrandCarousel.js
@@ -1,32 +1,17 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import CR7 from "../../../assets/brands/CR7.jfif";
+import Messi from "../../../assets/brands/Messi.jfif";
 
 const BrandCarousel = () => {
   return (
     <div>
       <Carousel>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
+          <img className="d-block w-100" src={CR7} alt="First slide" />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
+          <img className="d-block w-100" src={Messi} alt="Second slide" />
         </Carousel.Item>
       </Carousel>
     </div>
