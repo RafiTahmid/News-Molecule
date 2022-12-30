ef3cf30533ad268caedc5ae4c3b450e8a97c5c8c
RafiTahmid <rafi.tahmid019@gmail.com>
2022-12-30 15:07:34 +0600

    Social media added

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,7 +1,18 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
-import { FaGoogle, FaGithub } from "react-icons/fa";
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaYoutube,
+  FaTwitter,
+  FaWhatsapp,
+  FaDiscord,
+} from "react-icons/fa";
+import ListGroup from "react-bootstrap/ListGroup";
+import BrandCarousel from "../BrandCarousel/BrandCarousel";
+
 const RightSideNav = () => {
   return (
     <div>
@@ -13,6 +24,30 @@ const RightSideNav = () => {
           <FaGithub></FaGithub> Login with Github
         </Button>
       </ButtonGroup>
+
+      <div>
+        <h5 className="mt-4">Find us On</h5>
+        <ListGroup>
+          <ListGroup.Item className="mb-2">
+            <FaFacebook /> FaceBook
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaYoutube /> YouTube
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitter /> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaWhatsapp /> WhatsApp
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaDiscord /> Discord
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div>
+        <BrandCarousel />
+      </div>
     </div>
   );
 };
