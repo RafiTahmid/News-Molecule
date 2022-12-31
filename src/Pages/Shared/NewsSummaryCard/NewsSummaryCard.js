dd8f698f303eb99202c058172448f03938f9918e
RafiTahmid <rafi.tahmid019@gmail.com>
2022-12-31 19:16:55 +0600

    Styles added to the news card

--- src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -1,7 +1,48 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import Image from "react-bootstrap/Image";
+import { FaRegBookmark, FaShareAlt } from "react-icons/fa";
 
 const NewsSummaryCard = ({ news }) => {
-  return <div>{news.title}</div>;
+  const { _id, title, author, details, image_url, total_view } = news;
+
+  return (
+    <Card className="mb-5">
+      <Card.Header className="d-flex justify-content-between align-items-center">
+        <div className="d-flex">
+          <Image
+            roundedCircle
+            src={author?.img}
+            style={{ height: "60px" }}
+          ></Image>
+          <div className="ps-3">
+            <p className="mb-0">{author.name}</p>
+            <p>{author?.published_date}</p>
+          </div>
+        </div>
+        <div>
+          <FaRegBookmark className="me-2" />
+          <FaShareAlt />
+        </div>
+      </Card.Header>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Img src={image_url} alt="Card image" />
+        <Card.Text>
+          {details.length > 250 ? (
+            <p>
+              {details.slice(0, 250) + "..."}{" "}
+              <Link to={`/news/${_id}`}>[Continue Reading]</Link>
+            </p>
+          ) : (
+            <p>{details}</p>
+          )}
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+    </Card>
+  );
 };
 
 export default NewsSummaryCard;
