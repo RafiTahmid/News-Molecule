53a88fd1193bedb5c045c98fa925c16730300ad1
RafiTahmid <rafi.tahmid019@gmail.com>
2023-01-01 10:22:36 +0600

    <Button variant=primary>All From This Category</Button>

--- src/Pages/News/News/News.js
+++ src/Pages/News/News/News.js
@@ -1,10 +1,22 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Link, useLoaderData } from "react-router-dom";
 
 const News = () => {
+  const news = useLoaderData();
+  const { title, details, image_url, category_id } = news;
   return (
-    <div>
-      <h2>This is News</h2>
-    </div>
+    <Card>
+      <Card.Img variant="top" src={image_url} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{details}</Card.Text>
+        <Link to={`/category/${category_id}`}>
+          <Button variant="primary">All From This Category</Button>
+        </Link>
+      </Card.Body>
+    </Card>
   );
 };
 
